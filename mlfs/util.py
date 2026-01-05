0da216c24d634aff03000373dc31e37f589925b2
Alex <wikialex02@gmail.com>
2026-01-05 01:29:14 +0100

    compleated feature backfill

--- mlfs/util.py
+++ mlfs/util.py
@@ -16,6 +16,58 @@ import hsfs
 from pathlib import Path
 
 def get_historical_weather(city, start_date,  end_date, latitude, longitude):
+    # Setup the Open-Meteo API client with cache and retry on error
+    cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
+    retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+    openmeteo = openmeteo_requests.Client(session = retry_session)
+
+    # Make sure all required weather variables are listed here
+    # The order of variables in hourly or daily is important to assign them correctly below
+    url = "https://archive-api.open-meteo.com/v1/archive"
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "start_date": start_date,
+        "end_date": end_date,
+        "daily": ["weather_code", "apparent_temperature_mean", "daylight_duration", "precipitation_sum", "wind_speed_10m_max"]
+    }
+    responses = openmeteo.weather_api(url, params=params)
+
+    # Process first location. Add a for-loop for multiple locations or weather models
+    response = responses[0]
+    print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+    print(f"Elevation {response.Elevation()} m asl")
+    print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+    print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+    # Process daily data. The order of variables needs to be the same as requested.
+    daily = response.Daily()
+    weather_code = daily.Variables(0).ValuesAsNumpy()
+    apparent_temperature_mean = daily.Variables(1).ValuesAsNumpy()
+    daylight_duration = daily.Variables(2).ValuesAsNumpy()
+    precipitation_sum = daily.Variables(3).ValuesAsNumpy()
+    wind_speed_10m_max = daily.Variables(4).ValuesAsNumpy()
+
+    daily_data = {"date": pd.date_range(
+        start = pd.to_datetime(daily.Time(), unit = "s"),
+        end = pd.to_datetime(daily.TimeEnd(), unit = "s"),
+        freq = pd.Timedelta(seconds = daily.Interval()),
+        inclusive = "left"
+    )}
+
+    daily_data["weather_code"] = weather_code.astype('int32')
+    daily_data["apparent_temperature_mean"] = apparent_temperature_mean
+    daily_data["daylight_duration"] = daylight_duration
+    daily_data["precipitation_sum"] = precipitation_sum
+    daily_data["wind_speed_10m_max"] = wind_speed_10m_max
+
+    daily_dataframe = pd.DataFrame(data = daily_data)
+    daily_dataframe = daily_dataframe.dropna()
+    daily_dataframe['city'] = city
+
+    return daily_dataframe
+
+def get_historical_weather_aq(city, start_date,  end_date, latitude, longitude):
     # latitude, longitude = get_city_coordinates(city)
 
     # Setup the Open-Meteo API client with cache and retry on error
